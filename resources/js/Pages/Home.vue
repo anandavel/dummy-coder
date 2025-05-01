7a516e15e68f213ff252dff5f2c3f122ce36aed0
anandavel <32697457+anandavel@users.noreply.github.com>
2025-05-01 14:35:49 +0530

    init rep

---
+++ resources/js/Pages/Home.vue
@@ -0,0 +1,7 @@
+<template>
+    <div class="max-w-3xl mx-auto p-6 space-y-6">
+        <div class="text-blue-500 font-bold text-xl">
+            <p>Hello, World!</p>
+        </div>
+    </div>
+</template>
